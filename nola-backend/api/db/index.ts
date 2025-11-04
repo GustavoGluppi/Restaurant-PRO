018c56684a16855b0ff2037991af811d0360c999
Gustavo Gianluppi <gustavo.v.gianluppi@gmail.com>
2025-11-04 20:06:07 -0300

    Added Profile 'tools' to Data Gen Service

--- nola-backend/api/db/index.ts
+++ nola-backend/api/db/index.ts
@@ -3,10 +3,7 @@ import { Pool } from "pg";
 // Connecting postgres with pooling method
 const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: false,
 });
 
 export default pool;
